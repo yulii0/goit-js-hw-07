91c970d401770e4c315f01a262863ed61ecdd7f4
Yulii Kinash <122307040+yulii0@users.noreply.github.com>
2023-06-13 07:07:30 -0400

    HW 7 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+galleryList.addEventListener("click", onClick);
+console.log(createGalleryMarcup(galleryItems));
+
+const instance = basicLightbox.create(
+    `<img class="gallery__image-modal" src="" width="800" height="600">`,
+    {
+    onShow: (instance) => {
+        document.addEventListener("keydown", onBtnEscPush);
+    },
+    }
+);
+
+function onClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) {
+    return;
+    }
+    instance.element().querySelector("img").src = evt.target.dataset.source;
+    instance.show();
+}
+
+function onBtnEscPush(evt) {
+    if (evt.code === "Escape") {
+    instance.close();
+    }
+}
+
+function createGalleryMarcup(galleryItems) {
+    const elementGalleryMarcup = galleryItems
+    .map(({ description, original, preview }) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img 
+        class="gallery__image" 
+        src="${preview}" 
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </li>`;
+    })
+    .join("");
+    galleryList.insertAdjacentHTML("beforeend", elementGalleryMarcup);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+console.dir(createGalleryMarcup());
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  animationSpeed: 350,
+  captionDelay: 250,
+  captionsData: "alt",
+});
+
+function createGalleryMarcup() {
+  const elmGalleryMarcup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`;
+    })
+    .join("");
+  galleryList.insertAdjacentHTML("beforeend", elmGalleryMarcup);
+}
